e4369288472f5233b25f9b99e6462548617853dc
Jeffrey Nelson <nelsonjm@macpod.net>
2013-10-27 00:18:06 -0400

    adding message

--- lasershark_3d_printer_lib.c
+++ lasershark_3d_printer_lib.c
@@ -1,3 +1,23 @@
+/*
+lasershark_3d_printer_lib.h - Library to help 3d host apps talk with Lasershark devices.
+Copyright (C) 2012 Jeffrey Nelson <nelsonjm@macpod.net>
+
+This file is part of Lasershark's USB Host App.
+
+Lasershark is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Lasershark is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lasershark. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "lasershark_3d_printer_lib.h"
 
 int stepper_1_step_towards_home(libusb_device_handle *devh_ctl, uint32_t steps)
